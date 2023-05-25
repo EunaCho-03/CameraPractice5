af8b16f747ea92dba371d08df2cf03fac198713b
EunaCho-03 <karen.cho0827@gmail.com>
2023-05-25 18:57:01 +0900

    m

--- app/src/main/java/com/example/camerapractice5/MainActivity.java
+++ app/src/main/java/com/example/camerapractice5/MainActivity.java
@@ -1,23 +1,38 @@
 package com.example.camerapractice5;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.Image;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
+import android.widget.ImageView;
+import android.net.Uri;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.AspectRatio;
+import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+//import androidx.camera.core.
+import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
+
 import androidx.camera.video.MediaStoreOutputOptions;
 import androidx.camera.video.Quality;
 import androidx.camera.video.QualitySelector;
@@ -32,8 +47,11 @@ import androidx.core.content.ContextCompat;
 import com.example.camerapractice5.R;
 import com.google.common.util.concurrent.ListenableFuture;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
+import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -41,9 +59,13 @@ import java.util.concurrent.Executors;
 public class MainActivity extends AppCompatActivity {
     ExecutorService service;
     Recording recording = null;
+    ImageCapture imageCapture = null;
     VideoCapture<Recorder> videoCapture = null;
+    ProcessCameraProvider processCameraProvider;
+
     Button record, picture, flipCamera;
     PreviewView previewView;
+    ImageView imageView;
     int cameraFacing = CameraSelector.LENS_FACING_BACK;
     private final ActivityResultLauncher<String> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), result -> {
         if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
@@ -60,14 +82,62 @@ public class MainActivity extends AppCompatActivity {
         record = findViewById(R.id.record);
         picture = findViewById(R.id.picture);
         flipCamera = findViewById(R.id.flipCamera);
+        imageView = findViewById(R.id.imageView);
+
+        bindPreview();
+        bindImageCapture();
+
+        picture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    activityResultLauncher.launch(Manifest.permission.CAMERA);
+                }else if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    activityResultLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                }else{
+                    captureImage();
+                }
+                imageCapture.takePicture(ContextCompat.getMainExecutor(MainActivity.this),
+                        new ImageCapture.OnImageCapturedCallback() {
+                            @Override
+                            public void onCaptureSuccess(@NonNull ImageProxy image) {
+
+                                @SuppressLint({"UnsafeExperimentalUsageError", "UnsafeOptInUsageError"})
+                                Image mediaImage = image.getImage();
+                                Bitmap[] bitmap = {ImageUtil.mediaImageToBitmap(mediaImage)};
+
+                                Log.d("MainActivity", Integer.toString(bitmap[0].getWidth())); //4128
+                                Log.d("MainActivity", Integer.toString(bitmap[0].getHeight())); //3096
+
+                                Bitmap rotatedBitmap = ImageUtil.rotateBitmap(bitmap[0], image.getImageInfo().getRotationDegrees());
+
+                                Log.d("MainActivity", Integer.toString(rotatedBitmap.getWidth())); //3096
+                                Log.d("MainActivity", Integer.toString(rotatedBitmap.getHeight())); //4128
+                                Log.d("MainAtivity", Integer.toString(image.getImageInfo().getRotationDegrees())); //90 //0, 90, 180, 90 //이미지를 바르게 하기위해 시계 방향으로 회전해야할 각도
+
+
+                                imageView.setImageBitmap(rotatedBitmap);
+
+                                super.onCaptureSuccess(image);
+                                //String filename = "photo.JPG";
+                                //saveFile(filename);
+                                captureImage();
+
+                            }
+                        }
+                );
+            }
+        });
+
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            activityResultLauncher.launch(Manifest.permission.CAMERA);
+        } else {
+            startCamera(cameraFacing);
+        }
 
         record.setOnClickListener(view -> {
-            if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                activityResultLauncher.launch(Manifest.permission.CAMERA);
-            } else if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
                 activityResultLauncher.launch(Manifest.permission.RECORD_AUDIO);
-            } else if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                activityResultLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE);
             } else {
                 captureVideo();
             }
@@ -94,8 +164,46 @@ public class MainActivity extends AppCompatActivity {
         service = Executors.newSingleThreadExecutor();
     }
 
+    public void captureImage(){
+        Uri images;
+        ContentResolver contentResolver = getContentResolver();
+        images = MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY);
+
+        ContentValues contentValues = new ContentValues(0);
+        contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, System.currentTimeMillis()+ ".JPG");
+        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "images/");
+        Uri uri = contentResolver.insert(images, contentValues);
+
+        try{
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) imageView.getDrawable();
+            Bitmap bitmap = bitmapDrawable.getBitmap();
+
+            OutputStream outputStream = contentResolver.openOutputStream(Objects.requireNonNull(uri));
+            bitmap.compress(Bitmap.CompressFormat.JPEG,100, outputStream);
+            Objects.requireNonNull(outputStream);
+            Toast.makeText(getApplicationContext(), "갤러리에 저장 성공", Toast.LENGTH_LONG).show();
+
+        }catch(Exception e){
+            Toast.makeText(getApplicationContext(), "갤러리에 저장 실패", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+
+        //ImageCapture imageCapture1 = imageCapture;
+        /*
+        String name = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss-SSS", Locale.getDefault()).format(System.currentTimeMillis());
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, name);
+        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/*");
+        contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Picture");
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        Toast.makeText(this, "사진 저장", Toast.LENGTH_SHORT).show();
+        */
+
+    }
+
     public void captureVideo() {
-        //capture.setImageResource(R.drawable.round_stop_circle_24);
         Recording recording1 = recording;
         if (recording1 != null) {
             recording1.stop();
@@ -127,7 +235,6 @@ public class MainActivity extends AppCompatActivity {
                     String msg = "Error: " + ((VideoRecordEvent.Finalize) videoRecordEvent).getError();
                     Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
                 }
-                //capture.setImageResource(R.drawable.round_fiber_manual_record_24);
             }
         });
     }
@@ -160,6 +267,36 @@ public class MainActivity extends AppCompatActivity {
         }, ContextCompat.getMainExecutor(MainActivity.this));
     }
 
+    public byte[] bitmapToByteArray(Bitmap $bitmap){
+        ByteArrayOutputStream stream = new ByteArrayOutputStream() ;
+        $bitmap.compress( Bitmap.CompressFormat.JPEG, 100, stream) ;
+        return stream.toByteArray()  ;
+    }
+
+
+
+    void bindPreview() {
+        previewView.setScaleType(PreviewView.ScaleType.FIT_CENTER);
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(cameraFacing)
+                .build();
+        Preview preview = new Preview.Builder()
+                .setTargetAspectRatio(AspectRatio.RATIO_4_3) //디폴트 표준 비율
+                .build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        processCameraProvider.bindToLifecycle(this, cameraSelector, preview);
+    }
+
+    void bindImageCapture() {
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(cameraFacing)
+                .build();
+        imageCapture = new ImageCapture.Builder()
+                .build();
+
+        processCameraProvider.bindToLifecycle(this, cameraSelector, imageCapture);
+    }
 
 
     @Override
